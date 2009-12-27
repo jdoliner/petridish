2c9731b383150e75926f5a18e0692ed2c259bf59
Joe Doliner <jdoliner@Sebastian.(none)>
2009-12-27 06:04:08 -0500

    Subgraph swap breeding method now works with the new encoding.

--- graph/models.py
+++ graph/models.py
@@ -27,19 +27,22 @@ class Graph(Organism):
 		self.edges = ''
 		self.save()
 	def breed_subg_swp(self, mate):
-		selfv = self.v()
-		matev = mate.v()
+		selfv = range(self.num_v)
+		selfe = self.e()
+		matev = range(self.num_v, self.num_v + mate.num_v)
+		matee = mate.e()
+		matee = map(lambda e: (e[0] + self.num_v, e[1] + self.num_v), matee)
 		#select a subset of the vertices to induce subgraphs
 		selfsubg = random.sample(selfv, random.randrange(len(selfv))+1)
 		selfcomp = list(set(selfv) - set(selfsubg))
 		matesubg = random.sample(matev, random.randrange(len(matev))+1)
 		matecomp = list(set(selfv) - set(selfsubg))
 		#which edges are entirely internal
-		selfe_internal = self.e().filter(fr__in=selfsubg, to__in=selfsubg)
-		matee_internal = mate.e().filter(fr__in=selfsubg, to__in=selfsubg)
+		selfe_internal = filter(lambda e: e[0] in selfv and e[1] in selfv, selfe)
+		matee_internal = filter(lambda e: e[0] in matev and e[1] in matev, matee)
 		#edges that start in the subgraph but go outside
-		selfe_cross = self.e().filter(fr__in=selfsubg).filter(to__in=selfcomp) 
-		matee_cross = mate.e().filter(fr__in=matesubg).filter(to__in=matecomp)
+		selfe_cross = filter(lambda e: e[0] in selfv and not e[1] in selfv, selfe)
+		matee_cross = filter(lambda e: e[0] in matev and not e[1] in matev, matee)
 		#associations to reconnect the subgraphs; map from selfcomp->matesubg and matecomp->selfsubg
 		self_to_mate = {}
 		for i in range(len(selfcomp)):
@@ -48,12 +51,13 @@ class Graph(Organism):
 		for i in range(len(matecomp)):
 			mate_to_self[matecomp[i]] = selfsubg[i%len(selfsubg)]
 		for e in selfe_cross:
-			e.to = self_to_mate[e.to]
+			e[1] = self_to_mate[e[1]]
 		for e in matee_cross:
-			e.to = mate_to_self[e.to]
+			e[1] = mate_to_self[e[1]]
 		g = Graph()
-		g.dish = self.dish
-		g.G(list(selfsubg) + list(matesubg), list(selfe_internal) + list(matee_internal) + list(selfe_cross) + list(matee_cross))
+		g.init(self.dish, self.generation + 1)
+		g.num_v = len(selfsubg) + len(matesubg)
+		g.adde(selfe_internal + matee_internal + selfe_cross + matee_cross)
 	def random(self, size, p):
 		self.num_v = size
 		edges = []
